a32fdecf77b9fdb9cf879efd1aa46dc3793aea59
alexh7799 <hoerst.alexander@gmail.com>
2024-12-01 16:10:40 +0100

    finish summary.html

--- scripts/summary.js
+++ scripts/summary.js
@@ -1,5 +1,10 @@
+const BASE_URL =
+    "https://join-c39f7-default-rtdb.europe-west1.firebasedatabase.app/";
+
 function initSummary() {
-    generateGreets()
+    sessionStorage.setItem("userName", "Alex");
+    loadingNumbers();
+    generateGreets();
 }
 
 function generateGreets() {
@@ -7,11 +12,11 @@ function generateGreets() {
     let userName = sessionStorage.getItem("userName");
     let content = document.getElementById("greeting");
     content.innerHTML =
-      userName === ""
-        ? `<p>Good ${greetingTime}!</p>`
-        : `<p>Good ${greetingTime},</p>
+        userName === "Guest"
+            ? `<p>Good ${greetingTime}!</p>`
+            : `<p>Good ${greetingTime},</p>
         <p class="name-summary">${userName}</p>`;
-  }
+}
 
 function getGreeting() {
     let now = new Date();
@@ -23,5 +28,51 @@ function getGreeting() {
 }
 
 function goToBoard() {
-    window.location.href = 'board.html';
+    window.location.href = "board.html";
+}
+
+async function loadingNumbers() {
+    let response = await fetch(BASE_URL + ".json");
+    let tasks = await response.json();
+    let toDoNum = document.getElementById("toDoNum");
+    toDoNum.innerText = tasks.toDos.length;
+    let doneNum = document.getElementById("doneNum");
+    doneNum.innerText = tasks.done.length;
+    let allTasks = document.getElementById("allTasks");
+    allTasks.innerText =
+        tasks.toDos.length +
+        tasks.done.length +
+        tasks.progress.length +
+        tasks.awaiting.length;
+    let progressNum = document.getElementById("progressNum");
+    progressNum.innerText = tasks.progress.length;
+    let awaitNum = document.getElementById("awaitNum");
+    awaitNum.innerText = tasks.awaiting.length;
+    loadUrgentTasks(tasks);
+}
+
+function loadUrgentTasks(tasks) {
+    let urgentNum = document.getElementById("urgentNum");
+    let urgentDate = document.getElementById("urgentDate");
+    const urgentTasks = [];
+    for (const key in tasks) {
+        if (key !== "done" && Array.isArray(tasks[key]) && tasks) {
+            tasks[key].forEach((task) => {
+                if (task.prio === "urgent") {
+                    urgentTasks.push(task);
+                }
+            });
+        }
+    }
+    urgentTasks.sort((a, b) => {
+        const dateA = new Date(a.date.split("/").reverse().join("-"));
+        const dateB = new Date(b.date.split("/").reverse().join("-"));
+        return dateA - dateB;
+    });
+    urgentNum.innerText = urgentTasks.length;
+    if (urgentTasks.length === 0) {
+        urgentDate.innerText = "";
+    } else {
+        urgentDate.innerText = urgentTasks[0].date;
+    }
 }
