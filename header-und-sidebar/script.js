c18fccc43629fd52f3843052f02a9d01ee59cba7
Brook-Soul-King <lukas.schrooer.25@gmail.com>
2024-12-10 19:45:07 +0100

    profile pop up

---
+++ header-und-sidebar/script.js
@@ -0,0 +1,12 @@
+function togglePopUp() {
+    let popUp = document.getElementById("profile-pop-up");
+    popUp.classList.toggle("into-view");
+}
+
+function popUpTemplate() {
+    return `
+    <div class="">
+    
+    </div>
+    `;
+}
